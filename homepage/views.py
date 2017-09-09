67f31f62a03aa1fa9b7298fe057e56da505f2bbc
alexfernando <alex.fernando.801@gmail.com>
2017-09-09 17:54:19 +0000

    fix forms : register disabled , contact form set up for smtp gmail, deleting code that is not used in init.js.

--- homepage/views.py
+++ homepage/views.py
@@ -1,24 +1,32 @@
+from django.http import Http404, HttpResponse
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render,redirect
 from .forms import RegistrationForm,ContactForm
+from django.contrib import messages
+
 
 # Create your views here.
 
+#view for handling Register form
 def index(request):
+    contact_form = ContactForm
     if request.method == "POST":
-        register_form = RegistrationForm (request.POST)#haciendo post del form
-        if register_form.is_valid():
-            register_form.save()
+        registration_form = RegistrationForm (request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
             return render(request, 'homepage/success_register.html', {
-                'msg_register': register_form.data['name'] + ', hemos registrado tus datos con exito'})
-    else:
-        register_form = RegistrationForm()
-    return render(request, 'homepage/home.html', {'form_register': register_form})
+                'msg_register': registration_form.data['name'] + ', hemos registrado tus datos con exito'})
 
+    else:
+        registration_form = RegistrationForm()
+    return render(request, 'homepage/home.html', {'form_register': registration_form, 'form_contact':contact_form})
+
+#for handling contact form email
 def contact(request):
+    registration_form = RegistrationForm()
     if request.method == 'GET':
         contact_form = ContactForm()
+
     else:
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
@@ -28,13 +36,15 @@ def contact(request):
             message = contact_form.cleaned_data['message']
 
             try:
-                send_mail(subject,from_email,message,['alexf@localhost.com'])
+                send_mail(subject,message,from_email,['cep.201x@gmail.com'])
+
             except BadHeaderError:
                 return HttpResponse('Sus datos no se han guardado.')
 
             return redirect('success')
 
-    return render(request, 'homepage/contact_us.html', {'form_contact': contact_form})
+    return render(request, 'homepage/contact_us.html', {'form_contact': contact_form, 'form_register':registration_form})
 
 def success(request):
     return render(request,"homepage/success.html")
+
